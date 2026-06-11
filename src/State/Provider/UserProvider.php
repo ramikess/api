8bf4d3da00f36798c72849f72966daaabf3774f8
Rami <rkessentini@networth.fr>
2026-06-11 14:26:00 +0200

    get user : refacto

--- src/State/Provider/UserProvider.php
+++ src/State/Provider/UserProvider.php
@@ -10,41 +10,32 @@ use ApiPlatform\State\ProviderInterface;
 use App\ApiResource\UserResource;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\ObjectMapper\ObjectMapperInterface;
 
 final readonly class UserProvider implements ProviderInterface
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private ObjectMapperInterface $objectMapper,
     ) {}
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
-        // GetCollection
         if ($operation instanceof GetCollection) {
             $users = $this->entityManager->getRepository(User::class)->findAll();
 
-            return array_map(fn(User $user) => $this->toResource($user), $users);
+            return array_map(
+                fn(User $user) => $this->objectMapper->map($user, UserResource::class),
+                $users,
+            );
         }
 
-        // Get
         $user = $this->entityManager->getRepository(User::class)->find($uriVariables['id']);
 
         if ($user === null) {
             return null;
         }
 
-        return $this->toResource($user);
-    }
-
-    private function toResource(User $user): UserResource
-    {
-        $resource = new UserResource();
-        $resource->id = $user->getId();
-        $resource->firstName = $user->getFirstName();
-        $resource->lastName = $user->getLastName();
-        $resource->email = $user->getEmail();
-        $resource->photoUrl = $user->getPhoto() ? '/uploads/users/' . $user->getPhoto() : null;
-
-        return $resource;
+        return $this->objectMapper->map($user, UserResource::class);
     }
 }
